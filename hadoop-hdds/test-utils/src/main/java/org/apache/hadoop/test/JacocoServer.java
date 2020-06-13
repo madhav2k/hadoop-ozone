5fa15359902f7495ad05166fc4fd1699d75abf92
Elek Márton <elek@apache.org>
2020-06-13 23:57:38 +0200

    rat + checkstyle

--- hadoop-hdds/test-utils/src/main/java/org/apache/hadoop/test/JacocoServer.java
+++ hadoop-hdds/test-utils/src/main/java/org/apache/hadoop/test/JacocoServer.java
@@ -1,17 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.test;
-
-/*******************************************************************************
- * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
- * This program and the accompanying materials are made available under
- * the terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *    Marc R. Hoffmann - initial API and implementation
- *
- *******************************************************************************/
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -41,12 +45,6 @@ public final class JacocoServer {
   private JacocoServer() {
   }
 
-  /**
-   * Start the server as a standalone program.
-   *
-   * @param args
-   * @throws IOException
-   */
   public static void main(final String[] args) throws IOException {
     final ExecutionDataWriter fileWriter = new ExecutionDataWriter(
         new FileOutputStream(DESTFILE));
@@ -81,6 +79,7 @@ public final class JacocoServer {
 
     }
 
+    @SuppressWarnings("checkstyle:EmptyBlock")
     public void run() {
       try {
         while (reader.read()) {
